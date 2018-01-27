9b3aaf992b605debe30103a18388e1b786d15d6a
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-27 22:58:55 +0530

    gulp copy setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
+//Gulpfile
+//include gulp module
 const gulp = require('gulp');
-
+//setup file paths
 const paths = {
     jQuerySource: 'bower_components/jQuery/dist/jquery.js',
     materializeCssSource: 'bower_components/materialize/dist/css/materialize.css',
@@ -12,14 +14,26 @@ const paths = {
     distJS: 'public/js/'
 };
 
-gulp.task('css', function () {
+gulp.task('copyjQuery', function () {
+    return gulp.src(paths.jQuerySource).pipe(gulp.dest(paths.distJS));
+});
+
+gulp.task('copyMaterializeCss', function () {
+    return gulp.src(paths.materializeCssSource).pipe(gulp.dest(paths.distCSS));
+});
+
+gulp.task('copyMaterializeJs', function () {
+    return gulp.src(paths.materializeJsSource).pipe(gulp.dest(paths.distJS));
+});
+
+gulp.task('copyAllCustomCss', function () {
     return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.distCSS));
 });
 
-gulp.task('js', function () {
+gulp.task('copyAllCcustomJs', function () {
     return gulp.src(paths.srcJS).pipe(gulp.dest(paths.distJS));
 });
 
-gulp.task('default', ['css', 'js'], function () {
+gulp.task('default', ['copyjQuery', 'copyMaterializeCss', 'copyAllCustomCss', 'copyMaterializeJs', 'copyAllCcustomJs'], function () {
     console.log('Hello World!');
 });
