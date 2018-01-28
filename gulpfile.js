704e1b61f9aa2c61b6b083f74c162eebbeb7c15d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-28 13:50:25 +0530

    gulp refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 //Gulpfile
+'use strict';
 //include gulp module
 const gulp = require('gulp');
+//include gulp-sass
 const sass = require('gulp-sass');
 //setup file paths
 const paths = {
@@ -15,30 +17,35 @@ const paths = {
     distJS: 'public/js/'
 };
 
+//copy jQuery from bower_components to working directory
 gulp.task('copyjQuery', () => {
     return gulp.src(paths.jQuerySource).pipe(gulp.dest(paths.distJS));
 });
-
+//copy materialize.js from bower_components to working directory
 gulp.task('copyMaterializeJs', () => {
     return gulp.src(paths.materializeJsSource).pipe(gulp.dest(paths.distJS));
 });
-
+//copy custom javascript from working directory to dist
 gulp.task('copyAllCcustomJs', () => {
     return gulp.src(paths.srcJS).pipe(gulp.dest(paths.distJS));
 });
-
+//compile materialize scss surce files from bower_components to css and pipe to working directory
 gulp.task('compile-sass-css', () => {
     return gulp.src(paths.materializeSASS)
-        .pipe(sass())
+        .pipe(sass({
+            outputStyle: process.env.NODE_ENV === 'production' ? 'compressed' : ''
+        }).on('error', sass.logError))
         .pipe(gulp.dest(paths.distCSS));
 });
-
+//compile custom scss surce files from working directory to css and pipe to dist directory
 gulp.task('compile-source-sass', () => {
     return gulp.src(paths.srcSCSS)
-        .pipe(sass())
+        .pipe(sass({
+            outputStyle: process.env.NODE_ENV === 'production' ? 'compressed' : ''
+        }).on('error', sass.logError))
         .pipe(gulp.dest(paths.distCSS));
 });
-
+//setup and run tasks
 gulp.task('default', ['copyjQuery', 'copyMaterializeJs', 'copyAllCcustomJs', 'compile-sass-css', 'compile-source-sass'], () => {
     console.log('Gulp tasks completed successfully!');
 });
