a13da40ac36c98a29e8c96b21b3081c5592fd499
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-28 12:24:22 +0530

    gulp sass pipeline setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 //Gulpfile
 //include gulp module
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 //setup file paths
 const paths = {
     jQuerySource: 'bower_components/jQuery/dist/jquery.js',
-    materializeSASS:'bower_components/materialize/sass/**/*.scss',
+    materializeSASS: 'bower_components/materialize/sass/**/*.scss',
     materializeJsSource: 'bower_components/materialize/dist/js/materialize.js',
     srcJS: 'src/js/*.js',
     srcSCSS: 'src/scss/*.scss',
@@ -26,6 +27,18 @@ gulp.task('copyAllCcustomJs', () => {
     return gulp.src(paths.srcJS).pipe(gulp.dest(paths.distJS));
 });
 
-gulp.task('default', ['copyjQuery', 'copyMaterializeJs', 'copyAllCcustomJs'], () => {
+gulp.task('compile-sass-css', () => {
+    return gulp.src(paths.materializeSASS)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.distCSS));
+});
+
+gulp.task('compile-source-sass', () => {
+    return gulp.src(paths.srcSCSS)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.distCSS));
+});
+
+gulp.task('default', ['copyjQuery', 'copyMaterializeJs', 'copyAllCcustomJs', 'compile-sass-css', 'compile-source-sass'], () => {
     console.log('Gulp tasks completed successfully!');
 });
