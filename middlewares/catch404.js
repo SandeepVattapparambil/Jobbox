17031a265f06f26bc1d31c36bae15ac9e68b9ba5
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-02-04 15:42:59 +0530

    error middleware updates

--- middlewares/catch404.js
+++ middlewares/catch404.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+//catch 404
 router.use((req, res, next) => {
     let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+//handle error
+router.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error',{
+      message : err.message,
+      status: err.status,
+      stack: err.stack
+    });
+  });
 
 module.exports = router;
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,5 +2,4 @@ const express = require('express');
 const router = express.Router();
 //Middleware list
 router.use(require('./catch404'));
-router.use(require('./errorHandler'));
 module.exports = router;
